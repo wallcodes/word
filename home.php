2b514bf126c6a7517a8a9fdb97000bee448f863f
wallcodes <ernet82@gmail.com>
2022-09-29 15:58:08 +0200

    Agregados los archivos

---
+++ home.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login V10</title>
+    <!-- Recursos -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <link rel="stylesheet" type="text/css"
+        href="https://colorlib.com/etc/lf/Login_v10/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://colorlib.com/etc/lf/Login_v10/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://colorlib.com/etc/lf/Login_v10/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v10/vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://colorlib.com/etc/lf/Login_v10/vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v10/vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://colorlib.com/etc/lf/Login_v10/vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v10/css/util.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v10/css/main.css">
+    <meta name="robots" content="noindex, follow">
+    <link rel="stylesheet" href="src/css/home.css">
+    <link rel="stylesheet" href="src/css/footer.css">
+</head>
+
+<body>
+    <header class="p-3 mb-3 border-bottom">
+        <div class="container-fluid">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-center">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                        <use xlink:href="#bootstrap"></use>
+                    </svg>
+                </a>
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 link-secondary">Overview</a></li>
+                    <li><a href="#" class="nav-link px-2 link-dark">Inventory</a></li>
+                    <li><a href="#" class="nav-link px-2 link-dark">Customers</a></li>
+                    <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>
+                </ul>
+
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                    <input type="search" class="form-control" placeholder="Search..." aria-label="Search"
+                        control-id="ControlID-2">
+                </form>
+
+                <div class="dropdown text-end">
+                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                    </a>
+                    <ul class="dropdown-menu text-small">
+                        <li><a class="dropdown-item" href="#">Publicar.</a></li>
+                        <li><a class="dropdown-item" href="#">Ajustes</a></li>
+                        <li><a class="dropdown-item" href="#">Perfil</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="index.php">Salir</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12"></div>
+        </div>
+    </div>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 fixed-bottom">
+        <p class="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+
+        <a href="/"
+            class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32">
+                <use xlink:href="#bootstrap"></use>
+            </svg>
+        </a>
+
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+        </ul>
+    </footer>
+    <!-- Resources -->
+    <script src="https://colorlib.com/etc/lf/Login_v10/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script src="https://colorlib.com/etc/lf/Login_v10/vendor/animsition/js/animsition.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+    </script>
+    <script src="https://colorlib.com/etc/lf/Login_v10/vendor/select2/select2.min.js"></script>
+    <script src="https://colorlib.com/etc/lf/Login_v10/vendor/daterangepicker/moment.min.js"></script>
+    <script src="https://colorlib.com/etc/lf/Login_v10/vendor/daterangepicker/daterangepicker.js"></script>
+    <script src="https://colorlib.com/etc/lf/Login_v10/vendor/countdowntime/countdowntime.js"></script>
+    <script src="https://colorlib.com/etc/lf/Login_v10/js/main.js"></script>
+    <script defer
+        src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194"
+        integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw=="
+        data-cf-beacon='{"rayId":"752345afcab51946","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2022.8.1","si":100}'
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Registro</title>
+    <!-- Recursos -->
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="src/css/estilos.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <link rel="stylesheet" type="text/css"
+        href="https://colorlib.com/etc/lf/Login_v10/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://colorlib.com/etc/lf/Login_v10/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://colorlib.com/etc/lf/Login_v10/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v10/vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://colorlib.com/etc/lf/Login_v10/vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v10/vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://colorlib.com/etc/lf/Login_v10/vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v10/css/util.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/lf/Login_v10/css/main.css">
+    <meta name="robots" content="noindex, follow">
+    <style>
+    * {
+        margin: 0 auto;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="login100-form validate-form flex-sb flex-w">
+                <span class="login100-form-title p-t-20 p-b-5">
+                    Regístrate
+                </span>
+                <form class="login100-form validate-form flex-sb flex-w">
+                    <div class="col-6">
+                        <div class="wrap-login100 p-t-40">
+                            <div class="wrap-input100 validate-input m-b-16" data-validate="Cómo te llamas">
+                                <input class="input100" type="text" name="username" placeholder="Nombre">
+                                <span class="focus-input100"></span>
+                            </div>
+                            <div class="wrap-input100 validate-input m-b-16" data-validate="Debes indicar un apellido">
+                                <input class="input100" type="text" name="pass" placeholder="Apellido">
+                                <span class="focus-input100"></span>
+                            </div>
+                            <div class="wrap-input100 validate-input m-b-16" data-validate="Indica un correo electrónico">
+                                <input class="input100" type="mail" name="mail" placeholder="Mail">
+                                <span class="focus-input100"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="wrap-login100 p-t-40">
+                            <div class="wrap-input100 validate-input m-b-16" data-validate="Número de teléfono">
+                                <input class="input100" type="text" name="username" placeholder="Número de móvil">
+                                <span class="focus-input100"></span>
+                            </div>
+                            <div class="wrap-input100 validate-input m-b-16" data-validate="Nombre de usuario">
+                                <input class="input100" type="text" name="username" placeholder="Nombre de usuario">
+                                <span class="focus-input100"></span>
+                            </div>
+                            <div class="wrap-input100 validate-input m-b-16" data-validate="Establece una contraseña">
+                                <input class="input100" type="mail" name="password" placeholder="Contraseña">
+                                <span class="focus-input100"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <span class="login100-form-title p-t-20 p-b-5">
+                        <button class="login100-form-btn">
+                            Login
+                        </button>
+                        <a href="index.php" class="txt1" style="margin-top: 40px !important;">
+                            Forgot?
+                        </a>
+                    </span>
+                </form>
+            </div>
+        </div>
+        <div id="dropDownSelect1"></div>
+
+
+        <!-- Resources -->
+        <script src="https://colorlib.com/etc/lf/Login_v10/vendor/jquery/jquery-3.2.1.min.js"></script>
+        <script src="https://colorlib.com/etc/lf/Login_v10/vendor/animsition/js/animsition.min.js"></script>
+        <script src="https://colorlib.com/etc/lf/Login_v10/vendor/bootstrap/js/popper.js"></script>
+        <script src="https://colorlib.com/etc/lf/Login_v10/vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="https://colorlib.com/etc/lf/Login_v10/vendor/select2/select2.min.js"></script>
+        <script src="https://colorlib.com/etc/lf/Login_v10/vendor/daterangepicker/moment.min.js"></script>
+        <script src="https://colorlib.com/etc/lf/Login_v10/vendor/daterangepicker/daterangepicker.js"></script>
+        <script src="https://colorlib.com/etc/lf/Login_v10/vendor/countdowntime/countdowntime.js"></script>
+        <script src="https://colorlib.com/etc/lf/Login_v10/js/main.js"></script>
+        <script defer
+            src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194"
+            integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw=="
+            data-cf-beacon='{"rayId":"752345afcab51946","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2022.8.1","si":100}'
+            crossorigin="anonymous"></script>
+</body>
+
+</html>
